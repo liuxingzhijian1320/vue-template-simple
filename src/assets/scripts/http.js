34405ce66653cfe94fcece8068c7cf17d5436432
Zhooson <861077046@qq.com>
2017-12-03 16:37:01 +0800

    http的配置的增加解释说明

--- src/assets/scripts/http.js
+++ src/assets/scripts/http.js
@@ -1,31 +1,39 @@
 /**
- * http配置
+ * 封装axios的操作，拦截器
  */
-import axios from 'axios'
+import axios from 'axios';
 // import { MessageBox,Indicator } from 'mint-ui';
 
-axios.defaults.timeout = 5000
+//请求超时的操作（这里设置为5s的）的操作
+axios.defaults.timeout = 5000;
+
 // http请求拦截器
-
-axios.interceptors.request.use(config => {
-  Indicator.open({
-    text: 'Loading...',
-    spinnerType: 'fading-circle'
-  });
-  return config
-}, error => {
-  Indicator.close()
-  MessageBox.alert('加载超时')
-  return Promise.reject(error)
-})
+axios.interceptors.request.use(
+	config => {
+		Indicator.open({
+			text: 'Loading...',
+			spinnerType: 'fading-circle',
+		});
+		return config;
+	},
+	error => {
+		Indicator.close();
+		MessageBox.alert('加载超时');
+		return Promise.reject(error);
+	}
+);
 // http响应拦截器
-axios.interceptors.response.use(data => {// 响应成功关闭loading
-  Indicator.close()
-  return data
-}, error => {
-  Indicator.close()
-  MessageBox.alert('加载失败')
-  return Promise.reject(error)
-})
+axios.interceptors.response.use(
+	data => {
+		// 响应成功关闭loading
+		Indicator.close();
+		return data;
+	},
+	error => {
+		Indicator.close();
+		MessageBox.alert('加载失败');
+		return Promise.reject(error);
+	}
+);
 
-export default axios
+export default axios;
